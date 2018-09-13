5140bcd408c28ff4574cb2bce06f7459701559c3
Charles Reid <charlesreid1@gmail.com>
2018-09-13 08:40:49 -0700

    add console.log statements

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,8 +1,11 @@
 window.onload = function () {
+    console.log("Modifying tables");
     var tables = document.getElementsByTagName("table");
     var i;
     for(i = 0; i < tables.length; i++) {
+        console.log(i);
         tables[i].className += " table";
     }
+    console.log("Done");
 }
 
