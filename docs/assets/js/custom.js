aeafa17f62aee2fb4515f6f0bccebf83cc7db319
Charles Reid <charlesreid1@gmail.com>
2018-09-13 17:56:32 -0700

    nicer table style

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -3,6 +3,9 @@ window.onload = function () {
     var i;
     for(i = 0; i < tables.length; i++) {
         tables[i].className += " table";
+        tables[i].className += " table-striped";
+        tables[i].className += " table-hover";
+        tables[i].className += " table-bordered";
     }
 }
 
